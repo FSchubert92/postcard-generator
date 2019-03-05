a814379d4072622806e076a616a2eb12b3eb29fe
Florian Schubert <flo.schubert@me.com>
2019-03-05 16:31:14 +0100

    Add gradient at page end

--- src/Card/CardContainer.js
+++ src/Card/CardContainer.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 export default styled.section`
   display: grid;
   grid-auto-rows: auto;
-  grid-gap: 30px;
+  grid-gap: 80px;
   padding: 20px 0;
   overflow-y: scroll;
   scroll-snap-type: y mandatory;
--- src/app/App.js
+++ src/app/App.js
@@ -13,6 +13,15 @@ const Grid = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
+  &::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 18px;
+    background: linear-gradient(transparent, #eae6e6);
+  }
 `
 
 function App() {
