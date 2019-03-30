e9a5fb29e044394b065e59f55d9b4b9a7501c864
Florian Schubert <flo.schubert@me.com>
2019-03-30 17:27:23 +0100

    Make cards reducer work

--- client/src/NewCardForm/NewCardFormContainer.js
+++ client/src/NewCardForm/NewCardFormContainer.js
@@ -2,9 +2,9 @@ import { connect } from 'react-redux'
 import NewCardForm from './NewCardForm'
 import { createCard } from '../store/actions'
 
-const mapDispatchToProps = (state, ownProps) => ({
+const mapDispatchToProps = {
   onSubmit: createCard,
-})
+}
 
 export default connect(
   null,
