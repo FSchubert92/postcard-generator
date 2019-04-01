5d0d995be0106caa1e4a0bbbce543ede7fc038e6
Florian Schubert <flo.schubert@me.com>
2019-04-01 14:59:43 +0200

    Add basic thunk func

--- client/src/NewCardForm/NewCardFormContainer.js
+++ client/src/NewCardForm/NewCardFormContainer.js
@@ -1,9 +1,10 @@
 import { connect } from 'react-redux'
 import NewCardForm from './NewCardForm'
-import { createCard } from '../store/actions'
+import { createCard, getCards } from '../store/actions'
 
 const mapDispatchToProps = {
   onSubmit: createCard,
+  getCards,
 }
 
 export default connect(
--- client/src/store/actions.js
+++ client/src/store/actions.js
@@ -1,7 +1,16 @@
 import { createAction } from 'redux-starter-kit'
+import { getAllCards } from '../services'
 
 export const CREATE_CARD = 'CREATE_CARD'
 export const REMOVE_CARD = 'REMOVE_CARD'
+export const GET_CARDS_DONE = 'GET_CARDS_DONE'
 
 export const createCard = createAction(CREATE_CARD)
 export const removeCard = createAction(REMOVE_CARD)
+export const getCardsDone = createAction(GET_CARDS_DONE)
+
+export const getCards = () => dispatch => {
+  getAllCards().then(cards => {
+    dispatch(getCardsDone(cards))
+  })
+}
